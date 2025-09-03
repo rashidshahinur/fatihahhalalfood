686a08c7ed3b7a7cb1c1bc340cdf89fa37d49812
rashid.shahinur <lajuk_lengtu_chele@hotmail.com>
2025-09-03 13:52:11 -0700

    Menu price main page total 8 dishes

--- src/utils/food.js
+++ src/utils/food.js
@@ -18,6 +18,26 @@ const food = [
         id:4,
         name: 'Tandoori Chicken(1p) Set',
         price: '¥1200'
+    },
+    {
+        id:5,
+        name: 'Keema Curry Set',
+        price: '¥1000'
+    },
+    {
+        id:6,
+        name: 'Mutton Curry Set',
+        price: '¥1000'
+    },
+    {
+        id:7,
+        name: 'Vegetable Curry Set',
+        price: '¥850'
+    },
+    {
+        id:8,
+        name: 'Chicken Tikka(2pcs) Set',
+        price: '¥950'
     }
 ];
 

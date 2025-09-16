906b756969a1abc75ee29f02d3abb556b67083f5
rashid.shahinur <lajuk_lengtu_chele@hotmail.com>
2025-09-15 23:16:02 -0700

    Menu: change

--- src/utils/lunch.js
+++ src/utils/lunch.js
@@ -1,32 +1,32 @@
 const lunch = [
     {
         id: 1,
-        name: 'A Set-Curry Choice (Chicken, DAL, Vegetable)',
-        description: 'Nan, Salad, Soft Drink',
+        name: 'A Set-Curry Choice (Chicken, DAL, Vegetable) : Aセット – カレー選択（チキン・ダール・ベジタブル',
+        description: 'Choose from chicken, dal, or vegetable curry. Served with naan and side salad',
         price: '¥750'
     },
     {
         id: 2,
-        name: 'B Set-Curry Choice (Seafood, Sag Mutton, DAL Ghosh)',
-        description: 'Nan, Salad, Soft Drink',
+        name: 'B Set-Curry Choice (Seafood, Sag Mutton, DAL Ghosh) : Bセット – カレー選択（シーフード・サグマトン・ダールゴーシュト）',
+        description: 'Choose from seafood, sag mutton, or dal ghosh curry. Served with naan, side salad and soft drink',
         price: '¥850'
     },
     {
         id: 3,
-        name: 'Cheese Nan Set',
-        description: 'Curry Choice (Butter Chicken, DAL)',
+        name: 'Cheese Nan Set : チーズナンセット – カレー選択（バターチキン・ダール）',
+        description: 'Cheese-filled naan set with your choice of butter chicken or dal curry, served with salad',
         price: '¥950'
     },
     {
         id: 4,
-        name: 'Butter Chicken Curry Set',
-        description: 'Nan, Salad, Soft Drink',
+        name: 'Butter Chicken Curry Set : バターチキンカレーセット（ナン・サラダ・ソフトドリンク付き）',
+        description: 'Creamy butter chicken curry served with naan, side salad, and a soft drink of your choice',
         price: '¥900'
     },
     {
         id: 5,
-        name: 'Keema Curry Set',
-        description: 'Nan, Salad, Soft Drink',
+        name: 'Keema Curry Set : キーマカレーセット（ナン・サラダ・ソフトドリンク付き',
+        description: 'Spiced minced meat curry served with naan, side salad, and a soft drink of your choice',
         price: '¥900'
     },
     {

61fa054e27299dc242d9621718a9c035f59f2aef
rashid.shahinur <lajuk_lengtu_chele@hotmail.com>
2025-09-15 20:27:04 -0700

    Menu: change

--- src/utils/dinner.js
+++ src/utils/dinner.js
@@ -1,21 +1,56 @@
 const dinner = [
     {
         id: 1,
-        name: 'Spicy beef',
-        description: 'spicy beef, potatoes, carrots, cheese sauce, spices',
-        price: '17.50'
+        name: 'Chicken Curry Set',
+        description: 'Nan, Salad',
+        price: '¥950'
     },
     {
         id: 2,
-        name: 'Spaghetti bolognese',
-        description: 'onion, carrot, celery, minced meat, spaghetti, parmesan',
-        price: '15.00'
+        name: 'Butrer Chicken Curry Set',
+        description: 'Nan, Salad',
+        price: '¥1,000'
     },
     {
         id: 3,
-        name: 'Chickpea curry',
-        description: 'onion, chickpea, garlic, mushrooms, tomatoes, spices',
-        price: '12.25'
+        name: 'Dal Tadka Set',
+        description: 'Nan, Salad',
+        price: '¥900'
+    },
+    {
+        id: 4,
+        name: 'Tandoori Chicken(1pcs) Set',
+        description: 'Chicken Curry, Nan, Salad',
+        price: '¥1,200'
+    },
+    {
+        id: 5,
+        name: 'Mutton Curry Set',
+        description: 'Nan, Salad',
+        price: '¥1,000'
+    },
+    {
+        id: 6,
+        name: 'Keema Curry Set',
+        description: 'Nan, Salad',
+        price: '¥1,000'
+    },
+    {
+        id: 7,
+        name: 'Vegetable Curry Set',
+        description: 'Nan, Salad',
+        price: '¥850'
+    },
+    {
+        id: 8,
+        name: 'Chicken Tikka(2pcs) Set',
+        description: 'Nan, Salad',
+        price: '¥1,199'
+    },
+    {
+        id: 9,
+        description: '   All curries are available spiciness choice.',
+        price: 'Mild   --  Medium  --   Hot'
     }
 ];
 
